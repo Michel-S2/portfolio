af7bf33b2502225fc4d9b8c7212e4501a250bf73
michelF <freitasmichel1414@gmail.com>
2024-12-06 16:22:08 -0300

    scroll-icons

--- src/components/Sobre.tsx
+++ src/components/Sobre.tsx
@@ -5,6 +5,30 @@ import "./Animacao.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faMedal } from '@fortawesome/free-solid-svg-icons';
 
+type icones = {
+    id: number;
+    img: string
+}
+
+const icons:icones[] = [
+    {id: 1, img: "/images/icon1.png"},
+    {id: 2, img: "/images/icon2.png"},
+    {id: 3, img: "/images/icon3.png"},
+    {id: 4, img: "/images/icon4.png"},
+    {id: 5, img: "/images/icon5.png"},
+    {id: 6, img: "/images/icon6.png"},
+    {id: 7, img: "/images/icon7.png"},
+    {id: 8, img: "/images/icon1.png"},
+    {id: 9, img: "/images/icon2.png"},
+    {id: 10, img: "/images/icon3.png"},
+    {id: 11, img: "/images/icon4.png"},
+    {id: 12, img: "/images/icon5.png"},
+    {id: 13, img: "/images/icon6.png"},
+    {id: 14, img: "/images/icon7.png"},
+]
+
+const iconsDuplicate = icons.map(itens => [itens, {...icons}]);
+
 export const Sobre = () => {
     const ref = Animacao('slide-in-left', 'slide-in-left-active');
     const ref2 = Animacao('slide-in-right', 'slide-in-right-active');
@@ -28,6 +52,13 @@ export const Sobre = () => {
                         <p>Estou em busca da minha primeira oportunidade no mundo da programação, possuo os conhecimento das seguintes tecnologias: <span>ReactJs, TailwindCss, TypeScript</span></p>
                         <p>Estou em constante evolução de estudo e sempre querendo aprender mais </p> */}
                     </article>
+                </section>
+                <section className="container-scroll">
+                    <div className="scroll">
+                        {icons.map((item) => (
+                            <div className="scroll-item" key={item.id}><img className="icon" src={item.img} alt={`Icone ${item.img}`} /></div>
+                        ))}
+                    </div>
                 </section>
             </div>
         </section>
